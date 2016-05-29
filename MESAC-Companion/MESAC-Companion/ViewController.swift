1cd5252f4c2a773610c510e02cc221dda8fd625c
Philippos Slicher <philipposslicher@gmail.com>
2016-05-29 09:00:29 +0400

    no message

---
+++ MESAC-Companion/MESAC-Companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MESAC-Companion
+//
+//  Created by Philippos Slicher on 5/29/16.
+//  Copyright © 2016 YDPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
