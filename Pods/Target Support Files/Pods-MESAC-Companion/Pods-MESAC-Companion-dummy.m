ea592d444acd319e8e50f948934fc4e7c7213228
Philippos Slicher <philipposslicher@gmail.com>
2016-06-06 19:50:45 +0400

    Documentation start

--- Pods/Target Support Files/Pods-MESAC-Companion/Pods-MESAC-Companion-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MESAC_Companion : NSObject
-@end
-@implementation PodsDummy_Pods_MESAC_Companion
-@end
--- Pods/Target Support Files/SwiftPaginator/SwiftPaginator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftPaginator : NSObject
-@end
-@implementation PodsDummy_SwiftPaginator
-@end
