8e49b67d37463700e16a8edbf32e7d64ab9a7edb
Philippos Slicher <philipposslicher@gmail.com>
2016-08-23 19:50:39 +0400

    Fixed Warning

--- MESAC-Companion/AppDelegate.swift
+++ MESAC-Companion/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         //Load Controllers for Drawer
